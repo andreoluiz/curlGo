912a14021f532c5125788100903aa971f74b51ce
andreoluiz <95050579+andreoluiz@users.noreply.github.com>
2024-07-01 14:59:10 -0300

    Update main.go - Step 1 Done

--- curlGo/cliente/main.go
+++ curlGo/cliente/main.go
@@ -4,24 +4,60 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"net/url"
 	"os"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <URL>")
+		return
+	}
+
+	rawURL := os.Args[1]
+	if !strings.HasPrefix(rawURL, "http://") && !strings.HasPrefix(rawURL, "https://") {
+		rawURL = "http://" + rawURL
+	}
+
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return
+	}
+
+	fmt.Printf("Connecting to %s\n", rawURL)
+
+	conn, err := net.Dial("tcp", parsedURL.Hostname()+":80")
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
-		os.Exit(1)
+		return
 	}
 	defer conn.Close()
 
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter text: ")
-		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(conn, text+"\n")
-
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: " + message)
+	path := parsedURL.Path
+	if path == "" {
+		path = "/"
 	}
+
+	request := fmt.Sprintf("GET %s HTTP/1.1\r\n", path)
+	request += fmt.Sprintf("Host: %s\r\n", parsedURL.Hostname())
+	request += "Accept: */*\r\n"
+	request += "Connection: close\r\n\r\n"
+
+	fmt.Fprintf(conn, request)
+
+	reader := bufio.NewReader(conn)
+
+	statusLine, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+	fmt.Printf("Sending request GET %s HTTP/1.1\n", path)
+	fmt.Printf("Host: %s\n", parsedURL.Hostname())
+	fmt.Println("Accept: */*")
+
+	fmt.Printf("Response from server: %s\n", statusLine)
+
 }
